5b5b100afb9b2998c3b8860937dd87c4ef1fc7b2
Robert Coffie <rcoffie22@yahoo.com>
2020-08-02 19:02:17 +0100

    sunday2

--- account/views.py
+++ account/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from django.contrib import messages
 
 # Create your views here.
 
 def login(request):
+  if request.method == 'POST':
+    messages.error(request, 'testing error message')
+    return redirect ('login')
   return render(request,'account/login.html')
 
 
 
 def registration(request):
+  if request.method == 'POST':
+    messages.error(request,'testingerror message ma gee')
+    return redirect('register')
   return render(request,'account/register.html')
 
 
