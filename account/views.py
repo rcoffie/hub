efe8f9b8b43bc0a06ea12e23517131fd59bc544c
Robert Coffie <rcoffie22@yahoo.com>
2020-08-05 16:41:08 +0100

    mondey commit

--- account/views.py
+++ account/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 def login(request):
   if request.method == 'POST':
-    username  == request.POST['username']
-    password  == request.POST['password']
+    username = request.POST['username']
+    password  = request.POST['password']
     user = auth.authenticate(username=username,password=password)
     if user is not None:
       auth.login(request, user)
@@ -27,29 +27,29 @@ def registration(request):
     first_name = request.POST['first_name']
     last_name  = request.POST['last_name']
     username   = request.POST['username']
+    email      = request.POST['email']
+    username   = request.POST['username']
     password   = request.POST['password']
     password2  = request.POST['password2']
-    email      = request.POST['email']
 
     if password == password2:
-      if username == User.objects.filter(username=username).exists():
-        messages.error(request,'username already taken')
+      if User.objects.filter(username=username).exists():
+        messages.error(request, 'username already exists') 
         return redirect('register')
       else:
-        if email == User.objects.filter(email=email).exists():
-          messages.error(request,'this email has bee used try another one')
+        if User.objects.filter(email=email).exists():
+          messages.error(request, 'email already eixists')
           return redirect('register')
         else:
-          user = User.objects.create(first_name=first_name,last_name=last_name,username=username,password=password,email=email)
+          user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
           user.save()
+          messages.success(request, 'accounts created succesfuly you can login now')
           return redirect('login')
     else:
-      messages.error(request,'password dont match ')
+      messages.error(request, 'password dont match')
       return redirect('register')
 
-  else:
-
-   return render(request,'account/register.html')
+  return render(request, 'account/register.html')
 
 
   
